f7c652c1a2b08d6a0a1c5c124b390beea769b6e2
Ismail Suny <ismiletea@gmail.com>
2024-10-29 10:15:08 +0700

    App list grid view

--- app/src/main/java/com/bajiguri/bandrek/AppScreen/AppRepository.kt
+++ app/src/main/java/com/bajiguri/bandrek/AppScreen/AppRepository.kt
@@ -1,16 +1,10 @@
 package com.bajiguri.bandrek.AppScreen
 
 import android.content.Context
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
+import android.content.Intent
+import com.bajiguri.bandrek.utils.getBitmapFromDrawable
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
-
-data class AppInfo(
-    val name: String,
-    val packageName: String,
-    val icon: Int
-)
 
 class AppRepository @Inject constructor() {
     @Inject
@@ -18,15 +12,20 @@ class AppRepository @Inject constructor() {
     lateinit var context: Context
 
     fun getInstalledApps(): List<AppInfo> {
+        val intent = Intent(Intent.ACTION_MAIN, null)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
         val pm = context.packageManager
-        return pm
-            .getInstalledApplications(PackageManager.GET_META_DATA)
-            .map { appInfo ->
-                AppInfo(
-                    appInfo.loadLabel(pm).toString(),
-                    appInfo.packageName,
-                    appInfo.icon
-                )
-            }
+
+        return pm.queryIntentActivities(intent, 0).map {
+            val drawable = it.activityInfo.loadIcon(pm)
+            AppInfo(
+                name = it.loadLabel(pm).toString(),
+                activityName = it.activityInfo.name,
+                packageName = it.activityInfo.packageName,
+                drawable = drawable,
+                icon = getBitmapFromDrawable(drawable!!)
+            )
+
+        }
     }
 }
